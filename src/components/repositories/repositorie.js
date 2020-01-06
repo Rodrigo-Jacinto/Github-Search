1b252f6caed93e2f54f549afe2e586e5e6d26f43
Rodrigo Jacinto <rodrigojacinto16@gmail.com>
2020-01-06 15:31:17 -0300

    Update repositorie.js
    apagando código desnecessário do component repositorie

--- src/components/repositories/repositorie.js
+++ src/components/repositories/repositorie.js
@@ -31,33 +31,3 @@ const Repositorie = (props) => {
 
 export default Repositorie;
 
-/*
-export default class Repositories extends Component {
-
-   sortRepositories = (a, b) => {
-       return a.stargazers_count > b.stargazers_count ? -1 : a.stargazers_count < b.stargazers_count ? 1 : 0;
-   }
-
-
-   render() {
-       let repositories = this.props.repos;
-       repositories.sort(this.sortRepositories);
-
-       return (
-           repositories.map(repos =>
-               (
-                   <div className="mb-5" key={repos.id}>
-                       <h1 className="repo-name">{repos.name}</h1>
-                       <span className="repo-description">{repos.description}</span>
-                       <span className="star-count">
-                           <img src={starIcon} alt="star icon" />
-                           {repos.stargazers_count}
-                       </span>
-                   </div>
-
-               )
-           )
-       )
-   }
-}
-*/
